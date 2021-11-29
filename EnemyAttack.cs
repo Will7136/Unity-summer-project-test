407381f54ffacf1d21286f41c0b9804cc27ff28c
Will7136 <89648548+Will7136@users.noreply.github.com>
2021-11-29 10:59:37 +0000

    Create EnemyAttack.cs

---
+++ EnemyAttack.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    private Bandit player;
+    private Enemy[] Enemies;
+    public string SpecificEnemy;
+    
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        player = GameObject.FindObjectOfType<Bandit> ();
+        Enemies = GameObject.FindObjectsOfType<Enemy> ();
+        foreach (Enemy Enemy in Enemies){
+            if ((other.gameObject.tag == "Player") && (Enemy.EnemyType == SpecificEnemy)){
+                Enemy.AttackInRange();
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other){
+        player = GameObject.FindObjectOfType<Bandit> ();
+        Enemies = GameObject.FindObjectsOfType<Enemy> ();
+        foreach (Enemy Enemy in Enemies){
+            if ((other.gameObject.tag == "Player") && (Enemy.EnemyType == SpecificEnemy)){
+                Enemy.AttackOutOfRange();
+            }
+        }
+    }
+}
